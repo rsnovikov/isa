e0029d4d12d6bc5071b8b6db8da90ca3a90d1940
rsnovikov <job.novikovroman@gmail.com>
2023-12-03 13:06:02 +0300

    сделал 3 ЛР

--- src/EntryButton.tsx
+++ src/EntryButton.tsx
@@ -1,15 +1,19 @@
+import clsx from "clsx";
 import { FC } from "react";
+import { EntryType } from "./Lr3";
 
-interface IEntireButtonProps {
+interface IEntireButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	bottom?: number;
 	left?: number;
 	top?: number;
 	right?: number;
+	activeBtn?: EntryType;
 }
 
-export const EntryButton: FC<IEntireButtonProps> = ({ bottom, left, top, right }) => {
+export const EntryButton: FC<IEntireButtonProps> = ({ bottom, left, top, right, activeBtn, ...rest }) => {
 	return (
 		<button
+			{...rest}
 			className={`absolute w-[70px] h-[140px]`}
 			style={{
 				bottom,
@@ -18,8 +22,18 @@ export const EntryButton: FC<IEntireButtonProps> = ({ bottom, left, top, right }
 				right,
 			}}
 		>
-			<div className="absolute w-[70px] h-[70px] bg-red-600 bottom-0 left-0 animate-pulse" />
-			<div className="absolute w-[70px] h-[70px] bg-green-600 bottom-[70px] left-0" />
+			<div
+				className={clsx(
+					"absolute w-[70px] h-[70px] bg-red-700 bottom-0 left-0 border",
+					activeBtn === "exit" && "animate-pulse"
+				)}
+			/>
+			<div
+				className={clsx(
+					"absolute w-[70px] h-[70px] bg-green-700 bottom-[70px] left-0 border",
+					activeBtn === "entry" && "animate-pulse"
+				)}
+			/>
 		</button>
 	);
 };
--- src/Lr3.tsx
+++ src/Lr3.tsx
@@ -5,11 +5,52 @@ import { RoomTitle } from "./RoomTitle";
 
 export type Room = "A" | "B" | "C";
 
-export type ActiveEntiry = { room: Room; type: "entry" | "exit" } | null;
+export type EntryType = "entry" | "exit";
+
+export type ActiveEntry = { room: Room; type: EntryType } | null;
+
+const ENTRY_TIME = 3000;
 
 export const Lr3: FC = () => {
-	const [currentRooms, setCurrentRooms] = useState<Room[]>(["A", "B", "C"]);
-	const [activeEntry, setActiveEntry] = useState<ActiveEntiry>(null);
+	const [currentRooms, setCurrentRooms] = useState<Room[]>([]);
+	const [activeEntry, setActiveEntry] = useState<ActiveEntry>(null);
+
+	const onEntry = (room: Room) => {
+		if (currentRooms.includes(room)) {
+			setActiveEntry({ room, type: "exit" });
+			setTimeout(() => {
+				setCurrentRooms(prev => prev.filter(item => item !== room));
+				setActiveEntry(null);
+			}, ENTRY_TIME);
+		} else {
+			setActiveEntry({ room, type: "entry" });
+			setTimeout(() => {
+				setCurrentRooms(prev => [...prev, room]);
+				setActiveEntry(null);
+			}, ENTRY_TIME);
+		}
+	};
+
+	const onAEntryClick = () => {
+		if (currentRooms.includes("B") || currentRooms.includes("C") || activeEntry) {
+			return;
+		}
+		onEntry("A");
+	};
+
+	const onBEntryClick = () => {
+		if (!currentRooms.includes("A") || currentRooms.includes("C") || activeEntry) {
+			return;
+		}
+		onEntry("B");
+	};
+
+	const onCEntryClick = () => {
+		if (!currentRooms.includes("A") || currentRooms.includes("B") || activeEntry) {
+			return;
+		}
+		onEntry("C");
+	};
 
 	return (
 		<div>
@@ -25,26 +66,41 @@ export const Lr3: FC = () => {
 						)}
 					>
 						<RoomTitle>A</RoomTitle>
-						<EntryButton bottom={50} left={-70} />
+						<EntryButton
+							bottom={50}
+							left={-70}
+							activeBtn={activeEntry?.room === "A" ? activeEntry?.type : undefined}
+							onClick={onAEntryClick}
+						/>
 
 						<div
 							className={clsx(
 								"absolute w-[400px] h-[250px] top-0 right-[300px] border-2 border-gray-500 bg-gray-700",
-								currentRooms.includes("B") && "bg-green-800"
+								currentRooms.includes("C") && "bg-green-800"
 							)}
 						>
 							<RoomTitle>C</RoomTitle>
-							<EntryButton bottom={50} left={-70} />
+							<EntryButton
+								bottom={50}
+								left={-70}
+								onClick={onCEntryClick}
+								activeBtn={activeEntry?.room === "C" ? activeEntry?.type : undefined}
+							/>
 						</div>
 
 						<div
 							className={clsx(
 								"absolute w-[500px] h-[350px] bottom-0 right-0 border-2 border-gray-500 bg-gray-700",
-								currentRooms.includes("C") && "bg-green-800"
+								currentRooms.includes("B") && "bg-green-800"
 							)}
 						>
 							<RoomTitle>B</RoomTitle>
-							<EntryButton bottom={50} left={-70} />
+							<EntryButton
+								bottom={50}
+								left={-70}
+								onClick={onBEntryClick}
+								activeBtn={activeEntry?.room === "B" ? activeEntry?.type : undefined}
+							/>
 						</div>
 					</div>
 				</div>
